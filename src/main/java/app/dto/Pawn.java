1ea7057417ff7a7790e5124c59c969c37aeeb7f5
acer <wonverin@gmail.com>
2022-05-12 13:17:54 +0200

    "Zmiany w klasie Player, dodanie pól dla gracza"

--- src/main/java/app/dto/Pawn.java
+++ src/main/java/app/dto/Pawn.java
@@ -1,4 +1,5 @@
 package app.dto;
 
 public class Pawn {
+
 }
--- src/main/java/app/dto/Player.java
+++ src/main/java/app/dto/Player.java
@@ -2,14 +2,31 @@ package app.dto;
 
 public class Player {
     private String name;
-    private int budget;
+    int position = 0;
+    boolean inJail = false;
+    int timeInJail = 0;
+    private int money = 1500;
     public void setName(String name) {
         this.name = name;
     }
 
     public int getBudget() {
-        return budget;
+        return money;
     }
 
+    public int getPosition() {
+        return position;
+    }
 
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
