e67c5e036a67caf47f81f3a16fe655d6aaef88c3
acer <wonverin@gmail.com>
2022-05-12 13:06:01 +0200

    Dodanie logiki rzutu kostka dla gracza

--- src/main/java/app/controller/PlayerController.java
+++ src/main/java/app/controller/PlayerController.java
@@ -1,4 +1,15 @@
 package app.controller;
 
 public class PlayerController {
+    private double dice1,dice2;     //Zmienne na przetrzymywanie wartosci rzutu kostką
+    private boolean areDiceRolled;  //Zmienna po to by sprawdzać czy gracz już zakręcił kostką czy nie
+    private void rollADice(){
+        dice1 = Math.floor(Math.random() * 6) + 1;
+        dice2 = Math.floor(Math.random() * 6) + 1;
+        areDiceRolled = true;
+    }
+    private void resetDice(){
+        areDiceRolled = false;
+    }
+
 }
--- src/main/java/app/dto/Player.java
+++ src/main/java/app/dto/Player.java
@@ -1,4 +1,15 @@
 package app.dto;
 
 public class Player {
+    private String name;
+    private int budget;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+
 }
